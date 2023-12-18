a8e0e12219b08208f7d793aa4820376dd002a147
BlizzardDragon <blizzarddragon@list.ru>
2023-12-18 19:17:40 +0300

    fixed ServiceLocatorInstaller

--- Assets/Game/Scripts/System/ServiceLocatorInstaller.cs
+++ Assets/Game/Scripts/System/ServiceLocatorInstaller.cs
@@ -1,11 +1,37 @@
+using System;
+using System.Linq;
 using UnityEngine;
+
+public enum FindType
+{
+    GetComponentsInChildren,
+    FindObjectsOfType,
+}
 
 public class ServiceLocatorInstaller : MonoBehaviour
 {
+    [SerializeField] private FindType _findType;
+
+
     public void InstallServices()
     {
-        IService[] initListeners = GetComponentsInChildren<IService>();
-        
+        IService[] initListeners;
+
+        if (_findType == FindType.GetComponentsInChildren)
+        {
+            initListeners = GetComponentsInChildren<IService>();
+        }
+        else if (_findType == FindType.FindObjectsOfType)
+        {
+            initListeners = FindObjectsOfType<MonoBehaviour>()
+               .OfType<IService>()
+               .ToArray();
+        }
+        else
+        {
+            throw new Exception("Invalid search type");
+        }
+
         foreach (var listener in initListeners)
         {
             ServiceLocator.AddService(listener);
