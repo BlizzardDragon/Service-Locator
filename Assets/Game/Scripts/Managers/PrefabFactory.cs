abc7fee714074a3637922592e46eef3251d8d859
BlizzardDragon <blizzarddragon@list.ru>
2023-12-18 22:26:28 +0300

    added PrefabFactory

---
+++ Assets/Game/Scripts/Managers/PrefabFactory.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class PrefabFactory : MonoBehaviour, IService
+{
+    [SerializeField] private ParentsInfo[] _parentsInfo;
+
+    private PrefabCatalog _catalog;
+
+
+    public void Init(PrefabCatalog catalog)
+    {
+        _catalog = catalog;
+    }
+
+    public GameObject CreatePrefab(PrefabName key)
+    {
+        Transform targetParent = null;
+
+        foreach (var parentInfo in _parentsInfo)
+        {
+            if (parentInfo.Name == key)
+            {
+                targetParent = parentInfo.Parent;
+                break;
+            }
+        }
+
+        if (targetParent == null)
+        {
+            throw new Exception("Parent is not found");
+        }
+
+        return Instantiate(_catalog.GetPrefab(key), targetParent);
+    }
+}
+
+struct ParentsInfo
+{
+    public PrefabName Name;
+    public Transform Parent;
+}
